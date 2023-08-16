26429ccf7979f0701aac068d4a41a5e35db9b7c3
Pomprab BUREERAK <pbureera@paul-f4Br4s1.clusters.42paris.fr>
2023-08-16 13:00:57 +0200

    OK

--- ex00/Zombie.cpp
+++ ex00/Zombie.cpp
@@ -2,7 +2,7 @@
 
 Zombie::Zombie(std::string str) : name(str)
 {
-	std::cout << "I am " << name << ", and you are my meal!" << "\n" << std::endl;
+	std::cout << "I am " << name << ", and you are my meal!\n" << std::endl;
 }
 
 Zombie::~Zombie()
